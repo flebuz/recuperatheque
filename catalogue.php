1d87bbd31688ac99387b2ff091373bf2e95cd932
Dorian Timmermans <dorian.timmermans@gmail.com>
2019-07-23 11:59:53 +0200

    ajout de fonction de recherche et de tris au catalogue

--- catalogue.php
+++ catalogue.php
@@ -23,9 +23,38 @@
     <h1>[Nom de l'app]</h1>
   </div>
 
+  <form action="catalogue.php" method="GET">
+
+    <input type="text" class="w3-input" name="search">
+
+    <select class="w3-select" name="order">
+      <option value="date_ajout">Date de récupération</option>
+      <option value="qualite">Qualité</option>
+      <option value="nombre">Unités disponibles</option>
+    </select>
+
+    <input  class="w3-button search" type="submit" value="RECHERCHE" style="width:100%"/>
+
+  </form>
+
   <div class="item_container">
 
     <?php
+
+      //check les $_GET de recherche si valide
+        //check si l'option de recherche est valide
+      if (isset($_GET['search'])){
+        $recherche = '%'.htmlspecialchars($_GET['search']).'%';
+      } else{
+        $recherche = '';
+      }
+        //check si l'option de tri est parmis les choix valide
+      $tri_option = array('date_ajout', 'qualite', 'nombre');
+      if (isset($_GET['order']) && in_array($_GET['order'], $tri_option)){
+        $tri = htmlspecialchars($_GET['order']);
+      } else{
+        $tri = 'date_ajout';
+      }
 
       //connection database
       try{
@@ -36,7 +65,14 @@
       }
 
       //prep the request
-      $req = $bdd->prepare(' SELECT *, DATE_FORMAT(date_ajout, \'%d/%m/%Y\') AS date_ajout_fr FROM catalogue ORDER BY date_ajout DESC ');
+      $req = $bdd->prepare('  SELECT ID, categorie, sous_categorie, nombre, mesure, qualite, tags, DATE_FORMAT(date_ajout, \'%d/%m/%Y\') AS date_ajout_fr
+                              FROM catalogue
+                              WHERE categorie LIKE :search OR sous_categorie LIKE :search OR mesure LIKE :search OR tags LIKE :search OR description LIKE :search
+                              ORDER BY ' . $tri . ' DESC '
+                          );
+
+      //complete parametric values (note: column names are not values, and thus must be hardcoded into the query)
+      $req->bindValue(':search', '%' . $recherche . '%', PDO::PARAM_STR);
 
       //execute the request
       $req->execute();
