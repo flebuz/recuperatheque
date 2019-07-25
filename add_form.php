94f973741a09aa3782591d0ee699bfcfed329f0d
flebuz <47220010+flebuz@users.noreply.github.com>
2019-07-25 17:44:24 +0200

    Update add_form.php
    dérougification

--- add_form.php
+++ add_form.php
@@ -37,17 +37,17 @@
               <div class="row"></div>
               <!-- bouton upload photo -->
               <!--<label for="file">
-              <div class="btn-floating red lighten-1 waves-effect"><i class="material-icons photo-controls" title="Uploader une photo">cloud_upload</i></div>
+              <div class="btn-floating waves-effect"><i class="material-icons photo-controls" title="Uploader une photo">cloud_upload</i></div>
               </label>
               <input id="file" type="file" accept="image/*" capture style="display:none;">-->
 
               <!-- bouton prise de vue -->
               <div class="row">
-                <div  id="take-photo" title="Prendre un cliché" class="btn-floating btn-large red lighten-1 waves-effect invisible"><i class="material-icons photo-controls">camera_alt</i></div>
+                <div  id="take-photo" title="Prendre un cliché" class="btn-floating btn-large  waves-effect invisible"><i class="material-icons photo-controls">camera_alt</i></div>
 
               </div>
-              <!--<div  id="play" title="Activer la camera" class="btn-floating red lighten-1 waves-effect"><i class="material-icons photo-controls">play</i></div>
-              <div  id="stop" title="Stopper la camera" class="btn-floating red lighten-1 waves-effect"><i class="material-icons photo-controls">stop</i></div> -->
+              <!--<div  id="play" title="Activer la camera" class="btn-floating  waves-effect"><i class="material-icons photo-controls">play</i></div>
+              <div  id="stop" title="Stopper la camera" class="btn-floating  waves-effect"><i class="material-icons photo-controls">stop</i></div> -->
 
           </div>
 
@@ -60,7 +60,7 @@
 
         <div id="file_upload_container" class="col s8 m5 l5 center" style="position:relative">
                 <label for="file">
-                <div  id="upload-file-default" title="Prendre un cliché / Uploader une photo" class="btn-floating btn-large cam_btn_default red lighten-1 waves-effect "><i class="material-icons photo-controls">camera_alt</i></div>
+                <div  id="upload-file-default" title="Prendre un cliché / Uploader une photo" class="btn-floating btn-large cam_btn_default  waves-effect "><i class="material-icons photo-controls">camera_alt</i></div>
               </label>
               <input id="file" type="file" accept="image/*" capture style="display:none;">
         </div>
@@ -87,11 +87,11 @@
           <!--Les tabs reprenant les différentes catégories de matériaux -->
           <ul class="tabs z-depth-1">
             <li class="tab col s3 l2"><a class='dropdown-trigger btn-flat waves-effect' href="#bois" data-target='select-bois'>bois</a></li>
-            <li class="tab col s3 l2"><a class='dropdown-trigger btn-flat waves-effect red lighten-1' href="#metal" data-target='select-metal' style="color:#ffffff ">métal</a></li>
-            <li class="tab col s3 l2"><a class='dropdown-trigger btn-flat waves-effect red lighten-1' href="#papier" data-target='select-papier' style="color:#ffffff ">papier</a></li>
-            <li class="tab col s3 l2"><a class='dropdown-trigger btn-flat waves-effect red lighten-1' href="#plastique" data-target='select-plastique' style="color:#ffffff ">plastique</a></li>
-	          <li class="tab col s3 l2"><a class='dropdown-trigger btn-flat waves-effect red lighten-1' href="#verre" data-target='select-verre' style="color:#ffffff ">verre</a></li>
-	          <li class="tab col s3 l2"><a class='dropdown-trigger btn-flat waves-effect red lighten-1' href="#construction" data-target='select-construction' style="color:#ffffff ">construction</a></li>
+            <li class="tab col s3 l2"><a class='dropdown-trigger btn-flat waves-effect ' href="#metal" data-target='select-metal'>métal</a></li>
+            <li class="tab col s3 l2"><a class='dropdown-trigger btn-flat waves-effect ' href="#papier" data-target='select-papier'>papier</a></li>
+            <li class="tab col s3 l2"><a class='dropdown-trigger btn-flat waves-effect ' href="#plastique" data-target='select-plastique' >plastique</a></li>
+	          <li class="tab col s3 l2"><a class='dropdown-trigger btn-flat waves-effect ' href="#verre" data-target='select-verre'>verre</a></li>
+	          <li class="tab col s3 l2"><a class='dropdown-trigger btn-flat waves-effect ' href="#construction" data-target='select-construction'>construction</a></li>
 	          <li class="tab col s3 l2"><a href="#test4">textile</a></li> <!-- pas encore fonctionnel -->
 	          <li class="tab col s3 l2"><a href="#test4">quincaillerie</a></li>
 	          <li class="tab col s3 l2"><a href="#test4">mobilier</a></li>
@@ -118,63 +118,63 @@
 
       <!-- Ici les sous-catégories de matériaux qui s'affichent des les menus déroulants-->
       <ul id="select-bois" class="dropdown-content">
-    	  <li><a href="#!" style="color:#fe0002;">bois médium</a></li>
-    	  <li><a href="#!" style="color:#fe0002;">bois massif</a></li>
-    	  <li><a href="#!" style="color:#fe0002;">bois 3 plis</a></li>
-    	  <li><a href="#!" style="color:#fe0002;">OSB</a></li>
+    	  <li><a href="#!">bois médium</a></li>
+    	  <li><a href="#!">bois massif</a></li>
+    	  <li><a href="#!">bois 3 plis</a></li>
+    	  <li><a href="#!">OSB</a></li>
     	  <li class="divider" tabindex="-1"></li>
-    	  <li><a href="#!" style="color:#fe0002;"><i class="material-icons">more_horiz</i>Autres bois</a></li>
+    	  <li><a href="#!"><i class="material-icons">more_horiz</i>Autres bois</a></li>
       </ul>
       <ul id="select-metal" class="dropdown-content">
-    	  <li><a href="#!" style="color:#fe0002;">acier</a></li>
-    	  <li><a href="#!" style="color:#fe0002;">acier galvanisé</a></li>
-    	  <li><a href="#!" style="color:#fe0002;">fer à beton</a></li>
-    	  <li><a href="#!" style="color:#fe0002;">aluminium</a></li>
-    	  <li><a href="#!" style="color:#fe0002;">laiton</a></li>
+    	  <li><a href="#!">acier</a></li>
+    	  <li><a href="#!">acier galvanisé</a></li>
+    	  <li><a href="#!">fer à beton</a></li>
+    	  <li><a href="#!">aluminium</a></li>
+    	  <li><a href="#!">laiton</a></li>
     	  <li class="divider" tabindex="-1"></li>
-    	  <li><a href="#!" style="color:#fe0002;"><i class="material-icons">more_horiz</i>Autres métal</a></li>
+    	  <li><a href="#!"><i class="material-icons">more_horiz</i>Autres métal</a></li>
       </ul>
       <ul id="select-papier" class="dropdown-content">
-    	  <li><a href="#!" style="color:#fe0002;">papier</a></li>
-      	<li><a href="#!" style="color:#fe0002;">carton</a></li>
-      	<li><a href="#!" style="color:#fe0002;">fil de reliure</a></li>
+    	  <li><a href="#!">papier</a></li>
+      	<li><a href="#!">carton</a></li>
+      	<li><a href="#!">fil de reliure</a></li>
       	<li class="divider" tabindex="-1"></li>
-      	<li><a href="#!" style="color:#fe0002;"><i class="material-icons">more_horiz</i>Autres papetterie</a></li>
+      	<li><a href="#!"><i class="material-icons">more_horiz</i>Autres papetterie</a></li>
       </ul>
       <ul id="select-plastique" class="dropdown-content">
-      	<li><a href="#!" style="color:#fe0002;">plexiglas</a></li>
-      	<li><a href="#!" style="color:#fe0002;">dibon</a></li>
-      	<li><a href="#!" style="color:#fe0002;">mousse</a></li>
-      	<li><a href="#!" style="color:#fe0002;">PVC</a></li>
+      	<li><a href="#!">plexiglas</a></li>
+      	<li><a href="#!">dibon</a></li>
+      	<li><a href="#!">mousse</a></li>
+      	<li><a href="#!">PVC</a></li>
       	<li class="divider" tabindex="-1"></li>
-      	<li><a href="#!" style="color:#fe0002;"><i class="material-icons">more_horiz</i>Autres plastique</a></li>
+      	<li><a href="#!"><i class="material-icons">more_horiz</i>Autres plastique</a></li>
       </ul>
       <ul id="select-verre" class="dropdown-content">
-      	<li><a href="#!" style="color:#fe0002;">plaque de verre</a></li>
-      	<li><a href="#!" style="color:#fe0002;">miroir</a></li>
-      	<li><a href="#!" style="color:#fe0002;">verre martelé</a></li>
-      	<li><a href="#!" style="color:#fe0002;">récipient</a></li>
+      	<li><a href="#!">plaque de verre</a></li>
+      	<li><a href="#!">miroir</a></li>
+      	<li><a href="#!">verre martelé</a></li>
+      	<li><a href="#!">récipient</a></li>
       	<li class="divider" tabindex="-1"></li>
-      	<li><a href="#!" style="color:#fe0002;"><i class="material-icons">more_horiz</i>Autres verre</a></li>
+      	<li><a href="#!"><i class="material-icons">more_horiz</i>Autres verre</a></li>
       </ul>
       <ul id="select-construction" class="dropdown-content">
-      	<li><a href="#!" style="color:#fe0002;">béton</a></li>
-      	<li><a href="#!" style="color:#fe0002;">béton cellulaire</a></li>
-      	<li><a href="#!" style="color:#fe0002;">plâtre</a></li>
-      	<li><a href="#!" style="color:#fe0002;">carrelage</a></li>
-      	<li><a href="#!" style="color:#fe0002;">pierre</a></li>
-      	<li><a href="#!" style="color:#fe0002;">terre</a></li>
+      	<li><a href="#!">béton</a></li>
+      	<li><a href="#!">béton cellulaire</a></li>
+      	<li><a href="#!">plâtre</a></li>
+      	<li><a href="#!">carrelage</a></li>
+      	<li><a href="#!">pierre</a></li>
+      	<li><a href="#!">terre</a></li>
       	<li class="divider" tabindex="-1"></li>
-      	<li><a href="#!" style="color:#fe0002;"><i class="material-icons">more_horiz</i>Autres construction</a></li>
+      	<li><a href="#!"><i class="material-icons">more_horiz</i>Autres construction</a></li>
       </ul>
       <ul id="select-mobilier" class="dropdown-content">
-      	<li><a href="#!" style="color:#fe0002;">table</a></li>
-      	<li><a href="#!" style="color:#fe0002;">table a dessin</a></li>
-      	<li><a href="#!" style="color:#fe0002;">table à couture</a></li>
-      	<li><a href="#!" style="color:#fe0002;">vitrine</a></li>
-      	<li><a href="#!" style="color:#fe0002;">chevalet</a></li>
+      	<li><a href="#!">table</a></li>
+      	<li><a href="#!">table a dessin</a></li>
+      	<li><a href="#!">table à couture</a></li>
+      	<li><a href="#!">vitrine</a></li>
+      	<li><a href="#!">chevalet</a></li>
       	<li class="divider" tabindex="-1"></li>
-      	<li><a href="#!" style="color:#fe0002;"><i class="material-icons">more_horiz</i>Autre mobilier</a></li>
+      	<li><a href="#!"><i class="material-icons">more_horiz</i>Autre mobilier</a></li>
       </ul>
 
       <!-- Script requis par Materialize pour activer le composant Dropdown (qui sont définis en "visibility:hidden" trouvent tout en bas de index.php)-->
@@ -395,7 +395,7 @@ document.getElementById('indicateur_texte_usure').value="Erreur";
               <div class="col s4">
               </div>
         			<div class="col s4">
-        			 <a class="waves-effect waves-light btn-small red lighten-1" value="submit" onclick="document.getElementById('formulaire_encodage').submit(); document.getElementById('client').reset(); " >
+        			 <a class="waves-effect waves-light btn-small " value="submit" onclick="document.getElementById('formulaire_encodage').submit(); document.getElementById('client').reset(); " >
                  <i class="material-icons">thumb_up_alt</i>
                  Encoder
                </a>
