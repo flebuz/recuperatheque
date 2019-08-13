57bb75866fc4380e88af339fbe4aff360a5d5136
flebuz <47220010+flebuz@users.noreply.github.com>
2019-08-13 16:39:29 +0200

    Update add_form.js

--- js/add_form.js
+++ js/add_form.js
@@ -51,7 +51,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 function init_materialize()
 {
-
+  /* Script requis par Materialize pour activer le composant Dropdown (qui sont définis en "visibility:hidden" trouvent tout en bas de index.php)*/
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instance = M.Dropdown.init(elems, { coverTrigger: false, constrainWidth: false, outDuration:0});
 
