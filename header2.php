03120bf0132d44d377c26144035712355574b991
Dorian Timmermans <dorian.timmermans@gmail.com>
2019-09-19 16:50:14 +0200

    update

---
+++ header2.php
@@ -0,0 +1,10 @@
+
+<div class="header">
+
+  <div class="title">Mycélium</div>
+
+  <a href="catalogue.php"><i class="fas fa-book-open"></i></a>
+  <a href="add_form.php"><i class="fas fa-plus-circle"></i></a>
+  <a href="log.php"><i class="fas fa-user-lock"></i></a>
+
+</div>
