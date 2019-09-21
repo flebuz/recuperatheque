a60459ecfef41dd2508bb1b08c0893f4627f935a
Dorian Timmermans <dorian.timmermans@gmail.com>
2019-09-21 15:30:18 +0200

    affiche poids/dimension/remarques suelement si présent/necessaire

--- item_page2.php
+++ item_page2.php
@@ -49,6 +49,9 @@
     }
   ?>
 
+  <div class='item-single-back'>
+  <div class='item-single-container'>
+
   <?php
     //get the item
     $req = $bdd->prepare('  SELECT
@@ -63,152 +66,171 @@
     $req->bindValue(':id', $id, PDO::PARAM_INT);
     //execute the request
     $req->execute();
-
-    $item = $req->fetch();
   ?>
 
   <?php
-    // calcul du prix
-    if ($item['unitesscat']=='kg'){
-      $prix = $item['prixsscat'] * $item['poids'] / $item['pieces'];
+    if ($req->rowCount() > 0) {
+      $item = $req->fetch();
+
+      // calcul du prix
+      if ($item['unitesscat']=='kg'){
+        $prix = $item['prixsscat'] * $item['poids'] / $item['pieces'];
+      }
+      else{
+        $prix = $item['prixsscat'];
+      }
+      $prix = $prix * ($item['etat']/4);
+      //ou
+      $prix = $item['prix'];
+
+      //pluriel ou non sur le nombre de pièces
+      $piece = "1 pièce";
+      if ($item['pieces']>1){
+        $piece = $item['pieces'] . " pièces";
+      }
+
+      //divise les tags en list php
+      $tags = explode(", ",$item['tags']);
+
+      //dimensions dispo ou non
+      if ($item['dimensions']==''){
+        $dimensions='non-disponibles';
+      }
+      else{
+        $dimensions=$item['dimensions'];
+      }
+
+      //remarques ou pas
+      if ($item['remarques']==''){
+        $remarques='pas de remarques';
+      }
+      else{
+        $remarques=$item['remarques'];
+      }
+      ?>
+
+      <div class='item'>
+
+        <div class="item-photo-container">
+            <?php
+            echo '<img class="item-photo" src="photos/' . $item['ID_item'] . '.jpg" />'
+            ?>
+        </div>
+
+        <div class="item-text-container">
+
+          <div class='item-categorie-container'>
+            <!-- deux choix d'affichage si la sscat est Autre ou pas -->
+            <?php
+            if ($item['sous_categorie']!='Autre'){
+              ?>
+              <span class="item-souscategorie"> <?php echo $item['sous_categorie']; ?> </span>
+              <span class="item-categorie"> (<?php echo $item['categorie']; ?>) </span>
+            <?php
+            }
+            else{
+              ?>
+              <span class="item-souscategorie"> <?php echo $item['categorie']; ?> </span>
+            <?php
+            }
+            ?>
+
+          </div>
+
+          <div class="item-info-container">
+
+            <div class="item-info-line">
+              <i class='fas fa-coins item-icon'></i>
+              <div class="item-info"><?php echo $prix; ?> par pièce </div>
+            </div>
+
+            <div class="item-info-line">
+              <i class='fas fa-heart-broken item-icon'></i>
+              <div class="item-info"> État:
+                <span class='etat-icon-container'>
+                <?php
+                //echo $item['etat'];
+                for($n = 0; $n < 4; $n++){
+                  if($item['etat'] > $n){
+                    echo '<i class="fas fa-heart etat-icon"></i> ' ;
+                  } else{
+                    echo '<i class="far fa-heart etat-icon"></i> ' ;
+                  }
+                }
+                ?>
+                </span>
+              </div>
+            </div>
+
+            <div class="item-info-line">
+              <i class='fas fa-cubes item-icon'></i>
+              <div class="item-info"><?php echo $piece; ?></div>
+            </div>
+
+          </div>
+
+          <div class="item-info-plus-container">
+            <!-- les info qui apparaissent dans la page single -->
+
+            <?php
+              //poids seulement si c'est dans la sscat
+              if ($item['unitesscat']=='kg'){ ?>
+                <div class="item-info-line">
+                  <i class='fas fa-weight-hanging item-icon'></i>
+                  <div class="item-info"> <?php echo $item['poids']; ?> </div>
+                </div>
+              <?php
+              }
+            ?>
+
+            <div class="item-info-line">
+              <i class='fas fa-ruler item-icon'></i>
+              <div class="item-info"> <?php echo $dimensions; ?> </div>
+            </div>
+
+            <div class="item-info-line">
+              <i class='fas fa-info-circle item-icon'></i>
+              <div class="item-info"> <?php echo $remarques; ?> </div>
+            </div>
+
+          </div>
+
+          <div class='item-tags-container'>
+
+            <div class="item-info-line">
+              <i class='fas fa-tag item-icon'></i>
+              <div class="item-info">
+                <?php
+                  for($n = 0; $n < count($tags); $n++){
+                    echo '<a class="tag" href=#>#' . $tags[$n] . '</a>';
+                    if($n!=count($tags)-1){ echo ', '; }
+                  }
+                ?>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="item-date-container">
+
+            <div class="item-info-line">
+              <i class='far fa-calendar item-icon'></i>
+              <div class="item-info">récupéré le <?php echo $item['date_ajout_fr']; ?></div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <?php
     }
     else{
-      $prix = $item['prixsscat'];
-    }
-    $prix = $prix * ($item['etat']/4);
-    //ou
-    $prix = $item['prix'];
-
-    //pluriel ou non sur le nombre de pièces
-    $piece = "1 pièce";
-    if ($item['pieces']>1){
-      $piece = $item['pieces'] . " pièces";
-    }
-
-    //divise les tags en list php
-    $tags = explode(", ",$item['tags']);
-
-    //dimensions dispo ou non
-    if ($item['dimensions']==''){
-      $dimensions='non-disponibles';
-    }
-    else{
-      $dimensions=$item['dimensions'];
+      echo '<h3 class="w3-container"> Cet objet n\'existe pas </h3>';
     }
   ?>
 
-  <div class='item-single-back'>
-  <div class='item-single-container'>
-
-    <div class='item'>
-
-      <div class="item-photo-container">
-          <?php
-          echo '<img class="item-photo" src="photos/' . $item['ID_item'] . '.jpg" />'
-          ?>
-      </div>
-
-      <div class="item-text-container">
-
-        <div class='item-categorie-container'>
-          <!-- deux choix d'affichage si la sscat est Autre ou pas -->
-          <?php
-          if ($item['sous_categorie']!='Autre'){
-            ?>
-            <span class="item-souscategorie"> <?php echo $item['sous_categorie']; ?> </span>
-            <span class="item-categorie"> (<?php echo $item['categorie']; ?>) </span>
-          <?php
-          }
-          else{
-            ?>
-            <span class="item-souscategorie"> <?php echo $item['categorie']; ?> </span>
-          <?php
-          }
-          ?>
-
-        </div>
-
-        <div class="item-info-container">
-
-          <div class="item-info-line">
-            <i class='fas fa-coins item-icon'></i>
-            <div class="item-info"><?php echo $prix; ?> par pièce </div>
-          </div>
-
-          <div class="item-info-line">
-            <i class='fas fa-heart-broken item-icon'></i>
-            <div class="item-info"> État:
-              <span class='etat-icon-container'>
-              <?php
-              //echo $item['etat'];
-              for($n = 0; $n < 4; $n++){
-                if($item['etat'] > $n){
-                  echo '<i class="fas fa-heart etat-icon"></i> ' ;
-                } else{
-                  echo '<i class="far fa-heart etat-icon"></i> ' ;
-                }
-              }
-              ?>
-              </span>
-            </div>
-          </div>
-
-          <div class="item-info-line">
-            <i class='fas fa-cubes item-icon'></i>
-            <div class="item-info"><?php echo $piece; ?></div>
-          </div>
-
-        </div>
-
-        <div class="item-info-plus-container">
-          <!-- les info qui apparaissent dans la page single -->
-
-          <!-- //si dans sa categorie il y a le poids -->
-          <div class="item-info-line">
-            <i class='fas fa-weight-hanging item-icon'></i>
-            <div class="item-info"> <?php echo $item['poids']; ?> </div>
-          </div>
-
-          <div class="item-info-line">
-            <i class='fas fa-ruler item-icon'></i>
-            <div class="item-info"> <?php echo $dimensions; ?> </div>
-          </div>
-
-          <div class="item-info-line">
-            <i class='fas fa-info-circle item-icon'></i>
-            <div class="item-info"> <?php echo $item['remarques']; ?> </div>
-          </div>
-
-        </div>
-
-        <div class='item-tags-container'>
-
-          <div class="item-info-line">
-            <i class='fas fa-tag item-icon'></i>
-            <div class="item-info">
-              <?php
-                for($n = 0; $n < count($tags); $n++){
-                  echo '<a class="tag" href=#>#' . $tags[$n] . '</a>';
-                  if($n!=count($tags)-1){ echo ', '; }
-                }
-              ?>
-            </div>
-          </div>
-
-        </div>
-
-        <div class="item-date-container">
-
-          <div class="item-info-line">
-            <i class='far fa-calendar item-icon'></i>
-            <div class="item-info">récupéré le <?php echo $item['date_ajout_fr']; ?></div>
-          </div>
-
-        </div>
-
-      </div>
-
-    </div>
   </div>
   </div>
 </body>
