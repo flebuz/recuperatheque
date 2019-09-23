a7ac6a9ef0652fc4573a7f85affa68200e6378da
Dorian Timmermans <dorian.timmermans@gmail.com>
2019-09-23 16:50:31 +0200

    header dans un fichier different

--- catalogue.php
+++ catalogue.php
@@ -82,44 +82,55 @@
     }
   ?>
 
-  <!-- Bar de recherche - formulaire -->
-  <div class="search-bar-container">
-    <form class="search-bar" action="catalogue.php" method="GET">
 
-      <input type="text" class="w3-input search-bar-input" name="q" placeholder="Ajouter un mot clé à la recherche" value="<?php echo $query?>">
-      <button class="w3-xlarge fa fa-search search-bar-button" type="submit"></button>
+  <div class="content">
 
-      <?php
-        // on ajoute cat et sscat si jamais c'est déjà préciser
-        if($catsearch){
-          echo '<input type="hidden" name="catsearch" value="' . $catsearch . '"/>';
-        }
-        if($sscatsearch){
-          echo '<input type="hidden" name="sscatsearch" value="' . $sscatsearch . '"/>';
-        }
-      ?>
-
-    </form>
-  </div>
-
-  <div class="w3-row content">
-
-    <div class="w3-col s12 m3 l3 menu-container">
-
-      <!-- menu categorie et tri -->
-      <div class="w3-row menu-bar">
-        <button id="cat-button" class="w3-col s6 menu-title separation" onclick="openMenu(event,'categories')">Catégories
-          <i class='w3-medium fas fa-plus menu-icon'></i>
-        </button>
-        <button id="tri-button" class="w3-col s6 menu-title" onclick="openMenu(event,'tri')">Tri
-          <i class='w3-medium fas fa-sort menu-icon'></i>
-        </button>
+    <div class="w3-row">
+      <div class="w3-col s12 m3 l3">
       </div>
+      <div class="w3-col s12 m9 l9">
+        <div class="search-bar-container">
+          <form class="search-bar" action="catalogue.php" method="GET">
 
-      <?php include('categories_menu.php'); ?>
-      <?php include('tri_menu.php'); ?>
+            <input type="text" class="w3-input search-bar-input" name="q" placeholder="Ajouter un mot clé à la recherche" value="<?php echo $query?>">
+            <button class="w3-xlarge fa fa-search search-bar-button" type="submit"></button>
 
-      <script>
+            <?php
+              // on ajoute cat et sscat si jamais c'est déjà préciser
+              if($catsearch){
+                echo '<input type="hidden" name="catsearch" value="' . $catsearch . '"/>';
+              }
+              if($sscatsearch){
+                echo '<input type="hidden" name="sscatsearch" value="' . $sscatsearch . '"/>';
+              }
+            ?>
+
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="w3-row">
+
+      <div class="w3-col s12 m3 l3">
+
+        <!-- menu categorie et tri -->
+        <div class="menu-container">
+          <div class="w3-row menu-bar">
+            <button id="cat-button" class="w3-col s6 menu-title separation" onclick="openMenu(event,'categories')">Catégories
+              <i class='w3-medium fas fa-plus menu-icon'></i>
+            </button>
+            <button id="tri-button" class="w3-col s6 menu-title" onclick="openMenu(event,'tri')">Tri
+              <i class='w3-medium fas fa-sort menu-icon'></i>
+            </button>
+          </div>
+
+          <?php include('categories_menu.php'); ?>
+          <?php include('tri_menu.php'); ?>
+          
+        </div>
+
+        <script>
         function openMenu(evt,menuName) {
 
           var menu = document.getElementById(menuName);
@@ -151,92 +162,101 @@
             evt.currentTarget.className += " active";
           }
         }
-      </script>
+        </script>
 
-    </div>
+      </div>
 
-    <div class="w3-col s12 m9 l9">
+      <div class="w3-col s12 m9 l9">
 
-      <?php
-        //prep the request
-        //every lines is an item with joined categorie and subcategorie
-        $req = $bdd->prepare('  SELECT
-                                c.ID AS ID_item, c.ID_categorie, c.ID_souscategorie, c.pieces AS pieces, c.dimensions AS dimensions, c.etat AS etat, c.tags AS tags, c.prix AS prix, c.poids AS poids, c.remarques AS remarques, c.localisation AS localisation, DATE_FORMAT(c.date_ajout, \'%d/%m/%Y\') AS date_ajout_fr,
-                                cat.ID, cat.nom AS categorie,
-                                sscat.ID AS sscatID, sscat.ID_categorie, sscat.unite AS unitesscat, sscat.prix AS prixsscat, sscat.nom AS sous_categorie
-                                FROM catalogue c
-                                INNER JOIN categorie cat ON c.ID_categorie=cat.ID
-                                INNER JOIN souscategorie sscat ON c.ID_souscategorie=sscat.ID
-                                WHERE (cat.nom LIKE :search OR sscat.nom LIKE :search OR dimensions LIKE :search OR tags LIKE :search OR remarques LIKE :search)
-                                AND (c.ID_souscategorie = :sscatsearch OR :sscatsearch is null)
-                                AND (c.ID_categorie = :catsearch OR :catsearch is null)
-                                ORDER BY ' . $tri . ' DESC
-                            ');
 
-        //complete parametric values (note: column names are not values, and thus must be hardcoded into the query)
-        $req->bindValue(':search', '%' . $query . '%', PDO::PARAM_STR);
-        $req->bindValue(':sscatsearch', $sscatsearch, PDO::PARAM_INT);
-        $req->bindValue(':catsearch', $catsearch, PDO::PARAM_INT);
-
-        //execute la requete
-        $req->execute();
-        $number_results = $req->rowcount();
-       ?>
-
-      <!-- search resume -->
-      <?php
-        if($query != '' || $catsearch != 0){
-          //si une des deux condition est respacter on affiche le resumer
-          echo '<div class="search-resume">';
-
-          if($query != ''){
-            $getURL = '?' . http_build_query(array_merge($_GET, array('q'=>'')));
-            echo '<a href="' . $getURL . '">"' . $query . '"</a>';
-          }
-          if($sscatsearch != 0){
-            if($query != ''){ echo ' dans '; }
-            //convertit l'ID en nom
-            $getURL = '?' . http_build_query(array_merge($_GET, array('catsearch'=>0, 'sscatsearch'=>0)));
-            echo '<a href="' . $getURL . '">' . $system[$catsearch]['sscats'][$sscatsearch] . ' (' . $system[$catsearch]['nom'] . ')</a>';
-          }
-          elseif($catsearch != 0){
-            if($query != ''){ echo ' dans '; }
-            //convertit l'ID en nom
-            $getURL = '?' . http_build_query(array_merge($_GET, array('catsearch'=>0)));
-            echo '<a href="' . $getURL . '">' . $system[$catsearch]['nom'] . '</a>';
-          }
-          echo ' (' . $number_results . ')';
-          echo '</div>';
-        }
-      ?>
-
-      <div class="w3-row items-container">
-
+        <!-- search request -->
         <?php
-          if ($req->rowCount() > 0) {
-            while($item = $req->fetch()){
+          //prep the request
+          //every lines is an item with joined categorie and subcategorie
+          $req = $bdd->prepare('  SELECT
+          c.ID AS ID_item, c.ID_categorie, c.ID_souscategorie, c.pieces AS pieces, c.dimensions AS dimensions, c.etat AS etat, c.tags AS tags, c.prix AS prix, c.poids AS poids, c.remarques AS remarques, c.localisation AS localisation, DATE_FORMAT(c.date_ajout, \'%d/%m/%Y\') AS date_ajout_fr,
+          cat.ID, cat.nom AS categorie,
+          sscat.ID AS sscatID, sscat.ID_categorie, sscat.unite AS unitesscat, sscat.prix AS prixsscat, sscat.nom AS sous_categorie
+          FROM catalogue c
+          INNER JOIN categorie cat ON c.ID_categorie=cat.ID
+          INNER JOIN souscategorie sscat ON c.ID_souscategorie=sscat.ID
+          WHERE (cat.nom LIKE :search OR sscat.nom LIKE :search OR dimensions LIKE :search OR tags LIKE :search OR remarques LIKE :search)
+          AND (c.ID_souscategorie = :sscatsearch OR :sscatsearch is null)
+          AND (c.ID_categorie = :catsearch OR :catsearch is null)
+          ORDER BY ' . $tri . ' DESC
+          ');
 
-              //affichage de l'item
-              ?>
+          //complete parametric values (note: column names are not values, and thus must be hardcoded into the query)
+          $req->bindValue(':search', '%' . $query . '%', PDO::PARAM_STR);
+          $req->bindValue(':sscatsearch', $sscatsearch, PDO::PARAM_INT);
+          $req->bindValue(':catsearch', $catsearch, PDO::PARAM_INT);
 
-              <a class="item-link" href="item_page2.php?id=<?php echo $item['ID_item']?>">
-                <div class='w3-col s12 m6 l4'>
-                  <?php include('item.php');?>
-                </div>
-              </a>
+          //execute la requete
+          $req->execute();
+          $number_results = $req->rowcount();
+        ?>
 
-              <?php
+        <!-- search resume -->
+        <?php
+          if($query != '' || $catsearch != 0){
+            //si une des deux condition est respacter on affiche le resumer
+            echo '<div class="search-resume">';
+
+            if($query != ''){
+              $getURL = '?' . http_build_query(array_merge($_GET, array('q'=>'')));
+              echo '<a href="' . $getURL . '">"' . $query . '"</a>';
             }
-          }
-          else {
-            echo '<h3 class="w3-container"> Aucun résultat ne correspond à la recherche </h3>';
+            if($sscatsearch != 0){
+              if($query != ''){ echo ' dans '; }
+              //convertit l'ID en nom
+              $getURL = '?' . http_build_query(array_merge($_GET, array('catsearch'=>0, 'sscatsearch'=>0)));
+              echo '<a href="' . $getURL . '">' . $system[$catsearch]['sscats'][$sscatsearch] . ' (' . $system[$catsearch]['nom'] . ')</a>';
+            }
+            elseif($catsearch != 0){
+              if($query != ''){ echo ' dans '; }
+              //convertit l'ID en nom
+              $getURL = '?' . http_build_query(array_merge($_GET, array('catsearch'=>0)));
+              echo '<a href="' . $getURL . '">' . $system[$catsearch]['nom'] . '</a>';
+            }
+            echo ' (' . $number_results . ')';
+            echo '</div>';
           }
         ?>
 
+        <div class="w3-row items-container">
+
+          <?php
+            if ($req->rowCount() > 0) {
+              while($item = $req->fetch()){
+
+                //affichage de l'item
+                ?>
+
+                <a class="item-link" href="item_page2.php?id=<?php echo $item['ID_item']?>">
+                  <div class='w3-col s12 m6 l4'>
+                    <?php include('item.php');?>
+                  </div>
+                </a>
+
+                <?php
+              }
+            }
+            else {
+              echo '<h3 class="w3-container"> Aucun résultat ne correspond à la recherche </h3>';
+            }
+          ?>
+
+        </div>
+
       </div>
+
     </div>
 
   </div>
+
+
+  <!-- Bar de recherche - formulaire -->
+
 
 </body>
 
