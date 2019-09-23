20461840937ad15142fe098300909b4ddc03d550
Dorian Timmermans <dorian.timmermans@gmail.com>
2019-09-23 12:42:23 +0200

    le header/navbar indique la page sur laquelle on est

--- header2.php
+++ header2.php
@@ -2,11 +2,11 @@
 <div class="header-container">
   <div class="header">
 
-    <div class="title">Mycélium</div>
+    <div class="title"><a href="catalogue.php">Mycélium</a></div>
 
-    <a href="catalogue.php"><i class="fas fa-book-open"></i></a>
-    <a href="add_form.php"><i class="fas fa-plus-circle"></i></a>
-    <a href="log.php"><i class="fas fa-user-lock"></i></a>
+    <a href="catalogue.php" class="nav-icon <?php if(basename($_SERVER['PHP_SELF'])=="catalogue.php"){ echo "page-selected"; } ?>" ><i class="fas fa-book-open"></i></a>
+    <a href="add_form.php" class="nav-icon <?php if(basename($_SERVER['PHP_SELF'])=="add_form.php"){ echo "page-selected"; } ?>"><i class="fas fa-plus-circle"></i></a>
+    <a href="log.php" class="nav-icon <?php if(basename($_SERVER['PHP_SELF'])=="log.php"){ echo "page-selected"; } ?>"><i class="fas fa-user-lock"></i></a>
 
   </div>
 </div>
