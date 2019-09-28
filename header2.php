896c630272ae18669e47d38e144089db04c4a2c4
Dorian Timmermans <dorian.timmermans@gmail.com>
2019-09-28 16:52:50 +0200

    affiche nom des pages sur le header pour la version bureau

--- header2.php
+++ header2.php
@@ -4,20 +4,23 @@
 
     <div class="header-title"><a href="catalogue.php">Mycélium</a></div>
 
-    <a href="catalogue.php" class="nav-icon
+    <a href="catalogue.php" class="nav-button
       <?php if(in_array(basename($_SERVER['PHP_SELF']),array("catalogue.php","item_page2.php"))){
           echo "page-selected"; } ?>">
-      <i class="w3-large fas fa-book-open"></i>
+      <div class="nav-title">Catalogue</div>
+      <i class="nav-icon w3-large fas fa-book-open"></i>
     </a>
-    <a href="add_form.php" class="nav-icon
+    <a href="add_form.php" class="nav-button
       <?php if(basename($_SERVER['PHP_SELF'])=="add_form.php"){
           echo "page-selected"; } ?>">
-      <i class="w3-large fas fa-plus-circle"></i>
+      <div class="nav-title">Ajout</div>
+      <i class="nav-icon w3-large fas fa-plus-circle"></i>
     </a>
-    <a href="log.php" class="nav-icon
+    <a href="log.php" class="nav-button
       <?php if(basename($_SERVER['PHP_SELF'])=="log.php"){
           echo "page-selected"; } ?>">
-      <i class="w3-large fas fa-user-lock"></i>
+      <div class="nav-title">Admin</div>
+      <i class="nav-icon w3-large fas fa-user-lock"></i>
     </a>
 
   </div>
