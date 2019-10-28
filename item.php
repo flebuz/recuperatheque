2c0a4c9ae48f9cdd1a1e2698737552d9c1bdfc3b
Dorian Timmermans <dorian.timmermans@gmail.com>
2019-10-28 23:00:42 +0100

    réparation des tags comme lien vers une recherche

--- item.php
+++ item.php
@@ -122,7 +122,7 @@
           <div class="item-info">
             <?php
               for($n = 0; $n < count($tags); $n++){
-                echo '<a class="item-tag" href="catalogue?q=' . $tags[$n] . '">#' . $tags[$n] . '</a>';
+                echo '<a class="item-tag" href="catalogue.php?q=' . $tags[$n] . '">#' . $tags[$n] . '</a>';
                 if($n!=count($tags)-1){ echo ', '; }
               }
             ?>
