debc51994cf3abaa21c391511834f5e86a5a7637
flebuz <47220010+flebuz@users.noreply.github.com>
2019-11-13 15:15:04 +0100

    Update add_form.php

--- add_form.php
+++ add_form.php
@@ -2,7 +2,6 @@
 <html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 
-
 <?php
 // Prevent caching on this page to make sure it is always up-to-date
 // TO DO : Check if there is a less aggressive way to do it
