7292481820cb3b5f5ed5490682aa23cc8fbd67ed
Dorian Timmermans <dorian.timmermans@gmail.com>
2019-11-27 12:10:36 +0100

    javais oublié dajouté le footer lol

---
+++ footer.php
@@ -0,0 +1,36 @@
+
+<div class="footer-container">
+  <div class="footer">
+
+    <div>
+    <h4>Une Récupérathèque</h4>
+    <p>
+      est un <strong>magasin collaboratif</strong> de matériaux de <strong>réemploi</strong>
+      au sein d’une école de création (arts, architecture, design, stylisme, arts de la scène, etc)
+      fonctionnant avec sa propre monnaie ou son propre <strong>système d’échange</strong>,
+      et visant à favoriser la durabilité, la solidarité, et la création de <strong>lien social</strong>.
+    </p>
+    </div>
+
+    <div>
+    <h4>Le Mycélium</h4>
+    <p>
+      est l'appareil végétatif des <strong>champignons</strong>.</br>
+      Composé d'un ensemble de filaments ramifiés, il
+      forme un <strong>réseau souterrain</strong> entre les champignons et les plantes.</br>
+
+      Ces réseaux <strong>favorisent les transferts</strong> de nutriments,
+      notamment de la part de celui qui est en condition favorable (lumière, milieu nutritif)
+      et qui encourage la croissance de celui en condition défavorable.
+      Ces liens souterrain entre espèces différentes est vital dans le fonctionnement d'un <strong>écosystème</strong>.
+    </p>
+    </div>
+
+    <div class="colophon">
+      Web app développée par <strong>Dorian Timmermans</strong> et <strong>Maxime Lambrecht</strong>,
+      pour <a href="http://federation.recuperatheque.org/" target="_blank">la Fédération des Récupérathèques</a>.<br/>
+      Mozilla Public License 2.0.
+    </div>
+
+  </div>
+</div>
