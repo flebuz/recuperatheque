519a6f8edba81704b81ef88fb4ac7606329db7f3
flebuz <47220010+flebuz@users.noreply.github.com>
2023-03-15 16:46:07 +0100

    Add files via upload

---
+++ connection_db.php
@@ -0,0 +1,24 @@
+
+<?php
+//connection database
+try{
+   $bdd = new PDO('mysql:host=localhost;dbname=XXXXXXXXX;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+
+}
+catch(Exception $e){
+  die('Erreur : '.$e->getMessage());
+}
+?>
+
+<?php
+  //fonction pratique
+  function link_construct($params, $web_page = 1){
+    //crée un lien vers $web_page, dont les params GET sont ceux précisé dans $params + ceux déjà la
+    if ($web_page==1){
+      $web_page = basename($_SERVER['PHP_SELF']);
+    }
+    $getURL = $web_page . '?' . http_build_query(array_merge($_GET, $params));
+    return $getURL;
+  }
+?>
